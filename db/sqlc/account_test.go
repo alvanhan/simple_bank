5107670be5c1a473382a768e30730b962f8f5758
vanhan200902 <vanhan200902@gmail.com>
2023-09-10 14:10:08 +0700

    mock db go mock changed name test random currency

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -14,7 +14,7 @@ func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
-		Currency: util.RandomCurreny(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
